c5c9dfcf421a5e72101be946be64da2b11033428
Ryan <yysirius@189.cn>
2021-01-17 13:37:43 +0800

    feat:实现ArUco标签的绘制.

--- arucomarker.go
+++ arucomarker.go
@@ -1,6 +1,7 @@
 package arucomarker
 
 import (
+	"errors"
 	"fmt"
 	"image"
 	"image/color"
@@ -10,11 +11,14 @@ import (
 	"os"
 )
 
-const (
-	slideSize  = 150
-	markSize   = 5
-	borderSize = 1
-)
+// MarkerInfo 标签信息
+type MarkerInfo struct {
+	SlideSize  int
+	MarkerSize int
+	BorderSize int
+	Name       string
+	Data       []uint8
+}
 
 func drawBox(img *image.RGBA, x, y, size int, color color.Color) {
 	for dy := 0; dy < size; dy++ {
@@ -25,26 +29,40 @@ func drawBox(img *image.RGBA, x, y, size int, color color.Color) {
 }
 
 // DrawMarker 绘制标签
-func DrawMarker() error {
-	img := image.NewRGBA(image.Rect(0, 0, slideSize, slideSize))
+func DrawMarker(info MarkerInfo) error {
+	img := image.NewRGBA(image.Rect(0, 0, info.SlideSize, info.SlideSize))
 	draw.Draw(img, img.Bounds(), &image.Uniform{color.Black}, image.ZP, draw.Src)
 
-	slideCount := markSize + 2*borderSize
-	boxSize := slideSize / slideCount
-	fmt.Println(slideSize, markSize, borderSize, slideCount, boxSize)
-	index := 0
-	for dy := borderSize; dy < slideCount-borderSize; dy++ {
-		for dx := borderSize; dx < slideCount-borderSize; dx++ {
-			boxColor := color.Black
-			if index%2 == 0 {
-				boxColor = color.White
+	slideCount := info.MarkerSize + 2*info.BorderSize
+	boxSize := info.SlideSize / slideCount
+	if boxSize == 0 {
+		return errors.New("Slide size is too limit")
+	}
+	fmt.Printf("Draw marker size %dX%d width %d border %d\n", info.MarkerSize, info.MarkerSize, info.SlideSize, info.BorderSize)
+	byteIndex := 0
+	currentByte := info.Data[byteIndex]
+	bitIndex := 7
+	for dy := info.BorderSize; dy < slideCount-info.BorderSize; dy++ {
+		for dx := info.BorderSize; dx < slideCount-info.BorderSize; dx++ {
+			//fmt.Println(currentByte, byteIndex, bitIndex)
+			if (currentByte & (0x1 << bitIndex)) != 0 {
+				drawBox(img, dx*boxSize, dy*boxSize, boxSize, color.White)
+				//fmt.Println("set 1")
 			}
-			drawBox(img, dx*boxSize, dy*boxSize, boxSize, boxColor)
-			index++
+			if bitIndex == 0 {
+				byteIndex++
+				if byteIndex >= len(info.Data) {
+					break
+				}
+				currentByte = info.Data[byteIndex]
+				bitIndex = 7
+			} else {
+				bitIndex--
+			}
 		}
 	}
 
-	f, _ := os.Create("out.png")
+	f, _ := os.Create(fmt.Sprintf("%s.png", info.Name))
 	defer f.Close()
 	err := png.Encode(f, img)
 	if err != nil {
--- arucomarker_test.go
+++ arucomarker_test.go
@@ -5,10 +5,35 @@ import (
 )
 
 func TestDrawMarker(t *testing.T) {
-	err := DrawMarker()
-	if err != nil {
-		t.Log(err)
-	} else {
-		t.Log("test draw marker")
+	cases := []MarkerInfo{
+		{
+			SlideSize:  200,
+			MarkerSize: 5,
+			BorderSize: 1,
+			Name:       "5x5_0",
+			Data:       []uint8{162, 217, 94, 0, 82, 46, 217, 1},
+		},
+		{
+			SlideSize:  150,
+			MarkerSize: 7,
+			BorderSize: 1,
+			Name:       "7x7_8_150",
+			Data:       []uint8{137, 7, 31, 86, 150, 158, 0, 112, 74, 104, 49, 231, 125, 0},
+		},
+		{
+			SlideSize:  6,
+			MarkerSize: 7,
+			BorderSize: 1,
+			Name:       "7x7_8_10",
+			Data:       []uint8{137, 7, 31, 86, 150, 158, 0, 112, 74, 104, 49, 231, 125, 0},
+		},
 	}
+
+	for _, c := range cases {
+		err := DrawMarker(c)
+		if err != nil {
+			t.Fatal("draw failed", c.Name, err)
+		}
+	}
+
 }
